1c518677ee65ef8b3aad4c551b2fdb731d725787
Mitch LeBlanc <supermitch@gmail.com>
2017-12-06 20:31:03 -0800

    Add 2017 day 3 part B

--- 2017/03.py
+++ 2017/03.py
@@ -1,3 +1,4 @@
+# Part A:
 def find_square(n):
     # Squares are sized 1, 3, 5, 7, 9
     i = 1
@@ -41,15 +42,59 @@ def manhattan(n):
     return abs(x) + abs(y)
 
 
-if __name__ == '__main__':
+# Part B:
+def sum_neighbours(arr, coord):
+    """ Sum 9 nearest neighbours. """
+    x, y = coord
+    total = 0
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if (dx, dy) != (0, 0):
+                try:
+                    total += arr[(x + dx, y + dy)]
+                except KeyError:
+                    continue
+    return total
 
+
+def spiral_offsets():
+    """ Generate spiral step (dx, dy) values. """
+    size = 3
+    while True:
+        yield 1, 0   # right
+        for _ in range(size - 2):
+            yield 0, 1  # up
+        for _ in range(size - 1):
+            yield -1, 0  # left
+        for _ in range(size - 1):
+            yield 0, -1  # down
+        for _ in range(size - 1):
+            yield 1, 0  # right
+        size += 2
+
+
+def spiral_series(n):
+    x, y = (0, 0)
+    arr = {(x, y): 1}  # A dict is like an array but easy?
+    offsets = spiral_offsets()
+    while True:
+        dx, dy = next(offsets)
+        x, y = x + dx, y + dy
+        new_value = sum_neighbours(arr, (x, y))
+        if new_value > n:
+            return new_value
+        arr[(x, y)] = new_value
+
+
+if __name__ == '__main__':
     assert manhattan(1) == 0
-    assert manhattan(3) == 2
     assert manhattan(4) == 1
     assert manhattan(12) == 3
-    assert manhattan(20) == 3
     assert manhattan(23) == 2
     assert manhattan(17) == 4
     assert manhattan(1024) == 31
-
     print('Part A: {}'.format(manhattan(347991)))
+
+    assert spiral_series(6) == 10
+    assert spiral_series(800) == 806
+    print('Part B: {}'.format(spiral_series(347991)))
