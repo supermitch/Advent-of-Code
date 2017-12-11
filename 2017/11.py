636b682318dd32158f9b41aac3214cc504531a16
Mitch LeBlanc <supermitch@gmail.com>
2017-12-10 22:04:07 -0800

    Refactor 2017.11

--- 2017/11.py
+++ 2017/11.py
@@ -1,65 +1,14 @@
-#Part A
+def calculate_distance(row, col):
+    dc = abs(col)
+    dr = max(abs(row) - abs(col) * 0.5, 0)
+    return int(dr + dc)
 
-def part_a():
+
+def main():
     with open('11.input', 'r') as f:
         steps = f.read().strip().split(',')
 
-    col = 0
-    row = 0
-
-    dirori = {
-        'n': (1, 0),
-        's': (-1, 0),
-        'ne': (0.5, 1),
-        'nw': (0.5, -1),
-        'se': (-0.5, 1),
-        'sw': (-0.5, -1),
-    }
-    for step in steps:
-        dr, dc = dirori[step]
-        row += dr
-        col += dc
-
-    print(row, col)
-    print(calculate_distance(row, col))
-
-    count = 0
-    if col > 0:
-        dir = 'w'
-    elif col == 0:
-        dir = None
-    else:
-        dir = 'e'
-    if row > 0:
-        ori = 'n'
-    elif row == 0:
-        ori = None
-    else:
-        ori = 's'
-
-    for _ in range(abs(col)):
-        count += 1
-        # move nw
-        row += 0.5
-        col -= 1
-    print(row, col)
-
-    for _ in range(548):
-        row += 1
-    print(row, col)
-
-    print('Part A: ', 329 + 548)
-
-def calculate_distance(row, col):
-    import math
-    dc = abs(col)
-    dr = max(abs(row) - abs(col) * 0.5, 0)
-    return dr + dc
-
-# Part B
-def part_b(steps):
-
-    dirori = {
+    deltas = {
         'n': (1, 0),
         's': (-1, 0),
         'ne': (0.5, 1),
@@ -71,27 +20,15 @@ def part_b(steps):
     row, col = 0, 0
     max_dist = 0
     for step in steps:
-        dr, dc = dirori[step]
+        dr, dc = deltas[step]
         row += dr
         col += dc
 
-        dist = calculate_distance(row, col)
-        max_dist = max(dist, max_dist)
+        dist = calculate_distance(row, col)  # Part A
+        max_dist = max(dist, max_dist)  # Part B
 
-    print(max_dist)
-
-
-def main():
-    # Part A:
-    part_a()
-    print('Part A: {} - '.format(None))
-
-    # Part B:
-    # assert part_b(['se','sw','se','sw','sw']) == 3
-    with open('11.input', 'r') as f:
-        steps = f.read().strip().split(',')
-    part_b(steps)
-    print('Part B: {} - '.format(None))
+    print('Part A: {} - End position distance'.format(dist))
+    print('Part B: {} - Further distance'.format(max_dist))
 
 
 if __name__ == '__main__':
