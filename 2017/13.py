6f95bd992cd3dd2a72733dec4e85995e357830ac
Mitch LeBlanc <supermitch@gmail.com>
2017-12-12 22:26:21 -0800

    Can't fix 2017 day 13
    Can't figure it out. Logic is sound, no?

--- 2017/13.py
+++ 2017/13.py
@@ -9,12 +9,12 @@ class Scanner:
         if self.pos == self.range - 1 or self.pos == 0:
             self.dir *= -1
 
-    def __repr__(self):
-        return '{} of {}'.format(self.pos, self.range)
-
     def reset(self):
         self.pos = 0
         self.dir = +1
+
+    def __repr__(self):
+        return '{}/{}'.format(self.pos, self.range - 1)
 
 
 def reset(scanners):
@@ -39,24 +39,31 @@ def main():
     end = max(scanners.keys())
 
     for delay in range(20000):
-        reset(scanners)
+
         damage = 0
         caught = False
 
+        reset(scanners)
         for _ in range(delay):
             update(scanners)
 
         x = 0
+        print('\n')
+        print('Delay: ', delay)
         while x <= end:
             if x in scanners:
+                print('x', x, ' s')
                 s = scanners[x]
                 if s.pos == 0:  # contact
                     damage += x * s.range
                     caught = True
+                    print('damage!')
+            else:
+                print('x', x)
             update(scanners)
             x += 1
         if not caught:
-            print('Found delay of {}'.format(i))
+            print('Found delay of {}'.format(delay))
             break
 
 
