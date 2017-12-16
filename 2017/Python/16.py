a8331804958f132c2d9a17da8a7dde30a5bf74f8
Mitch LeBlanc <supermitch@gmail.com>
2017-12-15 23:01:23 -0800

    Refactor 2017 day 16

--- 2017/Python/16.py
+++ 2017/Python/16.py
@@ -14,33 +14,42 @@ def perform_dance(p, data):
     return p
 
 
+def cat(p):
+    return ''.join(p)
+
+
+def find_repeats(p, moves):
+    seen = []
+    while True:
+        p = perform_dance(p, moves)
+        if cat(p) not in seen:
+            seen.append(cat(p))
+        else:
+            return seen
+
+
 def main():
     with open('16.input', 'r') as f:
-        data = []
-        moves = f.readline().strip().split(',')
-        for move in moves:
+        moves = []
+        for move in f.readline().strip().split(','):
             type = move[0]
             if type == 'x':
                 pos = [int(x) for x in move[1:].split('/')]
             elif type == 'p':
-                pos = [x for x in move[1:].split('/')]
+                pos = move[1:].split('/')
             else:
                 pos = int(move[1:])
-            data.append((type, pos))
+            moves.append((type, pos))
 
     p = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
-    part_a = ''.join(perform_dance(p[:], data))
-    print('Part A {} - Program order after one dance'.format(part_a))
 
-    seen = []
-    for i in range(0, 100):
-        p = perform_dance(p, data)
-        if ''.join(p) not in seen:
-            seen.append(''.join(p))
+    part_a = cat(perform_dance(p[:], moves))
+    print('Part A {} - Order after 1 dance'.format(part_a))
 
-    idx = 1000000000 % len(seen)
-    part_b = ''.join(seen[idx - 1])
-    print('Part B {} - Program order after 1B dances'.format(part_b))
+    seen = find_repeats(p, moves)
+    idx = int(1e10) % len(seen) - 1
+    part_b = cat(seen[idx])
+    print('Part B {} - Order after 1e10 dances'.format(part_b))
 
 
 if __name__ == '__main__':
