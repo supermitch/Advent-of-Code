d4ee1f78c4c2b7d994d594c1f54ef9d6f60bc788
Mitch LeBlanc <supermitch@gmail.com>
2017-12-16 22:02:01 -0800

    Refactor 2017 day 17

--- 2017/Python/17.py
+++ 2017/Python/17.py
@@ -14,24 +14,24 @@ def part_a():
 def part_b():
     new_val = None
     pos = 0
-    for i in range(50000000):
-        idx = (pos + 301) % (i + 1)  # len(arr) == i + 1
+    for i in range(1, 50000000 + 1):
+        idx = (pos + 301) % i  # "len(arr)" == i
         if idx == 0:
-            new_val = i + 1  # insert new val
+            new_val = i  # "insert" new val
         pos = idx + 1
     return new_val
 
 
 def main():
     tic = time.time()
-    print('Part A: {} - '.format(part_a()))
-    print('Elapsed: {}'.format(time.time() - tic))
+    print('Part A: {} - Value after 2017 in circular buffer'.format(part_a()))
+    print('Elapsed: {:0.3f} s'.format(time.time() - tic))
 
     print('\n')
 
     tic = time.time()
-    print('Part B: {} - '.format(part_b()))
-    print('Elapsed: {}'.format(time.time() - tic))
+    print('Part B: {} - Value after 0, after 5e6 insertions'.format(part_b()))
+    print('Elapsed: {:0.3f} s'.format(time.time() - tic))
 
 
 if __name__ == '__main__':
