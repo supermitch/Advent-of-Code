09d388b4060688874d9e1d1939d67e6e8f973423
Mitch LeBlanc <mitch.leblanc@yardi.com>
2017-12-18 13:13:31 -0800

    Refactor 2017.18.B

--- 2017/Python/18.py
+++ 2017/Python/18.py
@@ -3,70 +3,48 @@ from collections import defaultdict
 
 def parse_line(line):
     d = line.strip().split()
-
+    reg = d[1] if d[1].isalpha() else int(d[1])  # Sometimes register is an int
     try:
-        reg = int(d[1])  # Sometimes register is an int
-    except ValueError:
-        reg = d[1]
-
-    try:
-        v = int(d[2])
-    except ValueError:
-        v = d[2]  # Sometimes value is a register
+        v = d[2] if d[2].isalpha() else int(d[2])  # Sometimes value is a register
     except IndexError:
         v = None
-
-    return d[0], reg, v  # Nominally: Type, register, value
+    return d[0], reg, v  # Nominally: Type, register, value | None
 
 
 def get_val(regs, val):
-    if isinstance(val, int):
-        return val
-    else:
-        return regs[val]
+    return val if isinstance(val, int) else regs[val]
 
 
-def run_commands(name, registers, data, pos, sent, received, send_count):
+def run_commands(name, registers, data, i, sent, received, send_count):
     skip = None
-    i = pos
-    waiting = False
     while True:
         if i >= len(data) or i < 0:
             break
-
-        d, reg, val = data[i]
+        d, r, v = data[i]
 
         if d == 'set':
-            registers[reg] = get_val(registers, val)
+            registers[r] = get_val(registers, v)
         elif d == 'add':
-            registers[reg] += get_val(registers, val)
+            registers[r] += get_val(registers, v)
         elif d == 'mul':
-            registers[reg] *= get_val(registers, val)
+            registers[r] *= get_val(registers, v)
         elif d == 'mod':
-            registers[reg] %= get_val(registers, val)
+            registers[r] %= get_val(registers, v)
         elif d == 'snd':
-            sent.append(registers[reg])
+            sent.append(registers[r])
             send_count += 1
         elif d == 'rcv':
             if not received:
-                waiting = True
-                break
-            registers[reg] = received.pop(0)
+                break  # Wait for messages
+            registers[r] = received.pop(0)
         elif d == 'jgz':
-            if isinstance(reg, int):
-                if reg > 0:
-                    skip = val
-            else:
-                if registers[reg] > 0:
-                    if isinstance(val, int):
-                        skip = val
-                    else:
-                        skip = registers[val]
+            if get_val(registers, r) > 0:  # r could be an int!
+                skip = get_val(registers, v)
 
         i += 1 if skip is None else skip
         skip = None
 
-    return i, sent, send_count, received, registers, waiting
+    return i, sent, send_count, received, registers
 
 
 def main():
@@ -86,13 +64,13 @@ def main():
 
     while True:
         if pos_a <= len(data):
-            pos_a, msg_a, count_a, msg_b, regs_a, waiting_a = run_commands('Prog A', regs_a, data, pos_a, msg_a, msg_b, count_a)
+            pos_a, msg_a, count_a, msg_b, regs_a = run_commands('Prog A', regs_a, data, pos_a, msg_a, msg_b, count_a)
         if pos_b <= len(data):
-            pos_b, msg_b, count_b, msg_a, regs_b, waiting_b = run_commands('Prog B', regs_b, data, pos_b, msg_b, msg_a, count_b)
+            pos_b, msg_b, count_b, msg_a, regs_b = run_commands('Prog B', regs_b, data, pos_b, msg_b, msg_a, count_b)
 
         if pos_a >= len(data) and pos_b >= len(data):  # Ran out of instructions
             break
-        if waiting_a and waiting_b and not msg_b and not msg_a:  # Deadlocked
+        if not msg_b and not msg_a:  # Deadlocked
             break
 
     print('Part B: {} - Messages sent by Program 1'.format(count_b))
