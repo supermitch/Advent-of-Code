6f5792d51d1775a5a2e4967f524314669b63e04c
Mitch LeBlanc <supermitch@gmail.com>
2017-12-17 23:23:41 -0800

    Refactor 2017.18.B

--- 2017/Python/18.py
+++ 2017/Python/18.py
@@ -1,28 +1,72 @@
-from collections import namedtuple, defaultdict, OrderedDict
-
-
-class Obj:
-    def __init__(self, val):
-        self.val = val
+from collections import defaultdict
 
 
 def parse_line(line):
     d = line.strip().split()
-    t = d[0]
 
     try:
-        reg = int(d[1])
+        reg = int(d[1])  # Sometimes register is an int
     except ValueError:
         reg = d[1]
 
     try:
         v = int(d[2])
     except ValueError:
-        v = d[2]
+        v = d[2]  # Sometimes value is a register
     except IndexError:
         v = None
 
-    return t, reg, v
+    return d[0], reg, v  # Nominally: Type, register, value
+
+
+def get_val(regs, val):
+    if isinstance(val, int):
+        return val
+    else:
+        return regs[val]
+
+
+def run_commands(name, registers, data, pos, sent, received, send_count):
+    skip = None
+    i = pos
+    waiting = False
+    while True:
+        if i >= len(data) or i < 0:
+            break
+
+        d, reg, val = data[i]
+
+        if d == 'set':
+            registers[reg] = get_val(registers, val)
+        elif d == 'add':
+            registers[reg] += get_val(registers, val)
+        elif d == 'mul':
+            registers[reg] *= get_val(registers, val)
+        elif d == 'mod':
+            registers[reg] %= get_val(registers, val)
+        elif d == 'snd':
+            sent.append(registers[reg])
+            send_count += 1
+        elif d == 'rcv':
+            if not received:
+                waiting = True
+                break
+            registers[reg] = received.pop(0)
+        elif d == 'jgz':
+            if isinstance(reg, int):
+                if reg > 0:
+                    skip = val
+            else:
+                if registers[reg] > 0:
+                    if isinstance(val, int):
+                        skip = val
+                    else:
+                        skip = registers[val]
+
+        i += 1 if skip is None else skip
+        skip = None
+
+    return i, sent, send_count, received, registers, waiting
 
 
 def main():
@@ -30,98 +74,28 @@ def main():
     with open('18.input', 'r') as f:
         for line in f:
             data.append(parse_line(line))
-    print(data)
 
-    test = [
-        'set a 1',
-        'add a 2',
-        'mul a a',
-        'mod a 5',
-        'snd a',
-        'set a 0',
-        'rcv a',
-        'jgz a -1',
-        'set a 1',
-        'jgz a -2',
-    ]
+    regs_a = defaultdict(int)
+    regs_a['p'] = 0
 
-    play_sound = None
-    recovered_sound = None
-    skip = None
+    regs_b = defaultdict(int)
+    regs_b['p'] = 1
 
-    registers = defaultdict(int)
-    for d, reg, v in data:
-        if isinstance(reg, int):
-            continue
-        registers[reg] = 0
+    pos_a, count_a, msg_a = 0, 0, []
+    pos_b, count_b, msg_b = 0, 0, []
 
-    i = 0
     while True:
-        if i >= len(data) or i < 0:
+        if pos_a <= len(data):
+            pos_a, msg_a, count_a, msg_b, regs_a, waiting_a = run_commands('Prog A', regs_a, data, pos_a, msg_a, msg_b, count_a)
+        if pos_b <= len(data):
+            pos_b, msg_b, count_b, msg_a, regs_b, waiting_b = run_commands('Prog B', regs_b, data, pos_b, msg_b, msg_a, count_b)
+
+        if pos_a >= len(data) and pos_b >= len(data):  # Ran out of instructions
             break
-        d, reg, val = data[i]
-        print(d, reg, val)
+        if waiting_a and waiting_b and not msg_b and not msg_a:  # Deadlocked
+            break
 
-        if isinstance(reg, int):
-            pass
-        else:
-            if reg not in registers:
-                registers[reg] = 0
-
-        try:
-            val = int(val)
-        except (TypeError, ValueError):
-            if val not in registers:
-                registers[val] = 0
-
-        if d == 'set':
-            if isinstance(val, int):
-                registers[reg] = val
-            else:
-                registers[reg] = registers[val]
-        elif d == 'add':
-            if isinstance(val, int):
-                registers[reg] += val
-            else:
-                registers[reg] += registers[val]
-        elif d == 'mul':
-            if isinstance(val, int):
-                registers[reg] *= val
-            else:
-                registers[reg] *= registers[val]
-        elif d == 'mod':
-            try:
-                if isinstance(val, int):
-                    registers[reg] %= val
-                else:
-                    registers[reg] %= registers[val]
-            except ZeroDivisionError:
-                print('Zero division')
-                registers[reg] = 0
-        elif d == 'snd':
-            play_sound = registers[reg]
-        elif d == 'rcv':
-            if registers[reg] > 0:
-                recovered_sound = play_sound if play_sound is not None else None
-                print('Recovered: ', recovered_sound)
-                break
-        elif d == 'jgz':
-            try:
-                reg = int(reg)
-            except ValueError:
-                if registers[reg] > 0:
-                    skip = val
-            else:
-                if reg > 0:
-                    skip = val
-        if skip:
-            i += skip
-            skip = None
-        else:
-            i += 1
-        print(registers)
-
-
+    print('Part B: {} - Messages sent by Program 1'.format(count_b))
 
 if __name__ == '__main__':
     main()
