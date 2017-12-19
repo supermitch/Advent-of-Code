7ddb93b9311b12268a9cea5975978d8ac7480394
Mitch LeBlanc <supermitch@gmail.com>
2017-12-19 08:14:49 -0800

    Refactor 2017 day 19

--- 2017/Python/19.py
+++ 2017/Python/19.py
@@ -2,41 +2,32 @@ import string
 
 
 def rotate(v, rotation):
-    if rotation == 'L':  # Positive 90 deg
-        return -v[1], v[0]
-    else:
-        return v[1], -v[0]
+    return (-v[1], v[0]) if rotation == 'L' else (v[1], -v[0])
 
 
-def make_turn(data, pos, dir):
-    x, y = pos
-    new_dir = rotate(dir, 'L')  # Try left
-    dx, dy = new_dir
-    nx, ny = x + dx, y + dy
-    if data[nx][ny] in '|-' + string.ascii_letters:
-        return new_dir
+def make_turn(data, x, y, dir):
+    dx, dy = rotate(dir, 'L')  # Try left first
+    new_x, new_y = x + dx, y + dy
+    if data[new_x][new_y] in '|-' + string.ascii_letters:  # Valid path
+        return dx, dy
     else:
         return rotate(dir, 'R')
 
 
 def main():
-    data = []
     with open('19.input', 'r') as f:
-        for line in f:
-            data.append(list(line.strip('\n')))
+        data = [list(line.strip('\n')) for line in f]
 
-    x, y = 0, data[0].index('|')
-    dir = (1, 0)  # Starting dir is downwards
+    x, y = 0, data[0].index('|')  # Start position
+    dir = (1, 0)  # Starting dir is downwards, call it +ve x-axis
 
-    seen = ''  # Seen alphabetical chars
+    seen = ''  # Seen letters
     steps = 0  # Step count
     while True:
         char = data[x][y]
         if char == '+':
-            dir = make_turn(data, (x, y), dir)
-        elif char in '|-':
-            pass  # Just keep swimming
-        elif char in string.ascii_letters:
+            dir = make_turn(data, x, y, dir)
+        elif char in string.ascii_letters:  # Includes uppercase
             seen += char
         elif char == ' ':
             break
