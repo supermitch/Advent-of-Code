3ce9664578c73b53394b74f31fb7705008287eb6
Mitch LeBlanc <supermitch@gmail.com>
2017-12-21 23:52:57 -0800

    Add 2015 day 2

---
+++ 2015/Python/02/02.py
@@ -0,0 +1,45 @@
+def area(rect):
+    x, y, z = rect
+    return 2 * x * z + 2 * x * y + 2 * z * y
+
+
+def slack(rect):
+    x, y, z = rect
+    return min([x * z, x * y, z * y])
+
+
+def part_a(rects):
+    total = 0
+    for rect in rects:
+        total += area(rect) + slack(rect)
+    return total
+
+
+def wrap(rect):
+    x, y, z = rect
+    perimeters = [2 * x + 2 * z, 2 * x + 2 * y, 2 * z + 2 * y]
+    return min(perimeters)
+
+
+def bow(rect):
+    x, y, z = rect
+    return x * y * z
+
+
+def part_b(rects):
+    total = 0
+    for rect in rects:
+        total += wrap(rect) + bow(rect)
+    return total
+
+
+def main():
+    with open('02.input', 'r') as f:
+        rects = [[int(d) for d in line.strip().split('x')] for line in f]
+
+    print('Part A: {} - Total square feet of wrapping paper'.format(part_a(rects)))
+    print('Part B: {} - Total length of ribbon'.format(part_b(rects)))
+
+
+if __name__ == '__main__':
+    main()
