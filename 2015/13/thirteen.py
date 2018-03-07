700898f9a66da4bb8b1dba178ed21358a751464e
Mitch LeBlanc <supermitch@gmail.com>
2018-03-07 00:06:27 -0800

    Add 2015.13 part 1 in Python

--- 2015/13/thirteen.py
+++ 2015/13/thirteen.py
@@ -1,14 +1,49 @@
-def find_optimum_happiness(guests)
-    return 10
+from itertools import permutations
+import pprint
+
+
+def parse_rules(line):
+    parts = line.strip().strip('.').split()
+    guest_a, guest_b = parts[0], parts[-1]
+    sign = 1 if parts[2] == 'gain' else -1
+    value = sign * int(parts[3])
+    return (guest_a + ':' + guest_b, value)
+
+
+def find_happiness(guests, rules):
+    total = 0
+    for i in range(len(guests)):
+        try:
+            k = guests[i] + ':' + guests[i + 1]  # Last one will fail
+        except IndexError:
+            k = guests[i] + ':' + guests[0]  # Loop around
+        total += rules[k]
+        # And other neighbour
+        try:
+            k = guests[i] + ':' + guests[i - 1]  # First one will fail
+        except IndexError:
+            k = guests[i] + ':' + guests[-1]  # Loop around
+        total += rules[k]
+    return total
 
 
 def main():
+    rules = {}
     with open('input.txt', 'r') as f:
-        lines = [l.strip() for l in f]
+        for l in f:
+            key, value = parse_rules(l)
+            rules[key] = value
 
-    guests = extract_guests(lines)
+    guests = set([k.split(':')[0] for k in rules])
 
-    part_a = find_optimum_happiness(guests)
+    max = 0
+    for i, perm in enumerate(permutations(guests, len(guests)), start=1):
+        value = find_happiness(perm, rules)
+        if value > max:
+            max = value
+        print(i, perm, value)
+
+    part_a = max
     print('Part A: {} - Optimum happiness'.format(part_a))
 
 
