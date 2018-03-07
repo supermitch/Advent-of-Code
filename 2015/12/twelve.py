b05d03cfac279ce1c0c493c2f7f741c2dd6b9d0e
Mitch LeBlanc <supermitch@gmail.com>
2018-03-06 22:14:03 -0800

    Remove useless accumulator from 2015.12

--- 2015/12/twelve.py
+++ 2015/12/twelve.py
@@ -1,37 +1,43 @@
 import json
 
 
-def traverse(obj, total, red_is_bad):
+def traverse(obj, red_is_bad):
     if isinstance(obj, str):
         return 0
     elif isinstance(obj, int):
         return obj
     elif isinstance(obj, list):
-        for o in obj:
-            total += traverse(o, 0, red_is_bad)
-        return total
+        return sum(traverse(o, red_is_bad) for o in obj)
     else:  # dict
         if red_is_bad and 'red' in obj.values():
             return 0  # This obj & children are not counted
         else:
-            for k, o in obj.items():
-                total += traverse(o, 0, red_is_bad)
-        return total
+            return sum(traverse(o, red_is_bad) for o in obj.values())
 
 
 def main():
-    test = {'j': {'a': 'orange', 'b': 'blue', 'c': {'t': 'red', 'j': 55}, 'd': 'violet', 'e': 133}, 'b': [1, 'cat', 2], 'c': 23}
-    assert(traverse(test, 0, False) == 214)
-    assert(traverse(test, 0, True) == 159)
+    test = {
+        'j': {
+            'a': 'orange',
+            'b': 'blue',
+            'c': {
+                't': 'red', 'j': 55
+            },
+            'd': 'violet',
+            'e': 133},
+        'b': [1, 'cat', 2],
+        'c': 23
+    }
+    assert(traverse(test, False) == 214)
+    assert(traverse(test, True) == 159)
 
     with open('input.txt', 'r') as f:
-        contents = list(f)
-    obj = json.loads(''.join(contents))
+        obj = json.loads(f.read())
 
-    part_a = traverse(obj, 0, False)
+    part_a = traverse(obj, False)
     print('Part A: {} - Sum of all number values'.format(part_a))
 
-    part_b = traverse(obj, 0, True)
+    part_b = traverse(obj, True)
     print('Part B: {} - Sum w/o red properties'.format(part_b))
 
 
