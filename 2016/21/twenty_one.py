5d58153208e0eee1d16c31fd533aa7479cbbe9c6
Mitch LeBlanc <supermitch@gmail.com>
2018-03-13 21:38:23 -0700

    Finish 2016.21.Python

--- 2016/21/twenty_one.py
+++ 2016/21/twenty_one.py
@@ -18,9 +18,11 @@ def parse(line):
         return ('rot', parts[-1], None)
 
 
-def run(rule, input):
+def run(rule, input, reverse=False):
     act, a, b = rule
     if act == 'mov':
+        if reverse:
+            a, b = b, a
         l = list(input)
         char_a = l.pop(a)
         l.insert(b, char_a)
@@ -41,24 +43,43 @@ def run(rule, input):
         input = input[:a] + mid[::-1] + input[b:]
     elif act == 'left':
         d = deque(input)
-        d.rotate(-a)
+        if reverse:
+            d.rotate(a)
+        else:
+            d.rotate(-a)
         input = ''.join(d)
     elif act == 'right':
         d = deque(input)
-        d.rotate(a)
+        if reverse:
+            d.rotate(-a)
+        else:
+            d.rotate(a)
         input = ''.join(d)
     elif act == 'rot':
         idx_a = input.find(a)
-        n = 1 + idx_a + (1 if idx_a >= 4 else 0)
         d = deque(input)
-        d.rotate(n)
+        if reverse:
+            n = {
+                0: -1,
+                1: -1,
+                2: 2,
+                3: -2,
+                4: 1,
+                5: -3,
+                6: 0,
+                7: -4,
+            }[idx_a]
+            d.rotate(n)
+        else:
+            n = 1 + idx_a + (1 if idx_a >= 4 else 0)
+            d.rotate(n)
         input = ''.join(d)
     return input
 
 
 def main():
     input = 'abcde'
-    f = [
+    test = [
         'swap position 4 with position 0',
         'swap letter d with letter b',
         'reverse positions 0 through 4',
@@ -68,24 +89,29 @@ def main():
         'rotate based on position of letter b',
         'rotate based on position of letter d',
     ]
-    rules = [parse(l.strip()) for l in f]
-    assert run(rules[0], 'abcde') == 'ebcda'
-    assert run(rules[1], 'ebcda') == 'edcba'
-    assert run(rules[2], 'edcba') == 'abcde'
-    assert run(rules[3], 'abcde') == 'bcdea'
-    assert run(rules[4], 'bcdea') == 'bdeac'
-    assert run(rules[5], 'bdeac') == 'abdec'
-    assert run(rules[6], 'abdec') == 'ecabd'
-    assert run(rules[7], 'ecabd') == 'decab'
+    test_rules = [parse(l.strip()) for l in test]
+    assert run(test_rules[0], 'abcde') == 'ebcda'
+    assert run(test_rules[1], 'ebcda') == 'edcba'
+    assert run(test_rules[2], 'edcba') == 'abcde'
+    assert run(test_rules[3], 'abcde') == 'bcdea'
+    assert run(test_rules[4], 'bcdea') == 'bdeac'
+    assert run(test_rules[5], 'bdeac') == 'abdec'
+    assert run(test_rules[6], 'abdec') == 'ecabd'
+    assert run(test_rules[7], 'ecabd') == 'decab'
+
 
     with open('input.txt', 'r') as f:
         rules = [parse(l.strip()) for l in f]
 
-    start = 'abcdefgh'
-    password = start
+    password = 'abcdefgh'
     for rule in rules:
         password = run(rule, password)
     print('Part A: {} - Scrambled password'.format(password))
+
+    password = 'fbgdceah'
+    for rule in rules[::-1]:
+        password = run(rule, password, reverse=True)
+    print('Part B: {} - Unscrambled password'.format(password))
 
 
 if __name__ == '__main__':
