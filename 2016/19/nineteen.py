4752dd596f0df7076de1716f71dc91aed8f60d22
Mitch LeBlanc <supermitch@gmail.com>
2018-04-06 22:51:18 -0700

    Refactor 2016.19.A.Python

--- 2016/19/nineteen.py
+++ 2016/19/nineteen.py
@@ -2,33 +2,27 @@ def next_elf(id, playing, no_of_elves):
     while True:
         id += 1
         id = id % (no_of_elves + 1)
-        id = max(id, 1)
-        if id in playing:
+        id = max(id, 1)  # Elf indices start at 1
+        if id in playing:  # This elf in the game?
             return id
+
+
+def part_a(playing, no_of_elves):
+    id = 1  # Starts at elf 1
+    while True:
+        next_id = next_elf(id, playing, no_of_elves)
+        if next_id == id:
+            return id
+        playing.remove(next_id)  # Next elf loses all presents: they're out
+        id = next_elf(next_id, playing, no_of_elves)
 
 
 def main():
-    # 1830115 -> too low
     no_of_elves = 3012210
-    circle = [1] * no_of_elves
-    elf_ids = range(1, no_of_elves + 1)
+    playing = set(range(1, no_of_elves + 1))
 
-    playing = set(elf_ids)
-    gifts = {x: 1 for x in elf_ids}
-
-    id = 1  # Starts at elf 1
-    while True:
-        # print('Elf {}'.format(id))
-        next_id = next_elf(id, playing, no_of_elves)
-        # print('Next elf {}'.format(next_id))
-        if next_id == id:
-            print('Only elf <{}> remains!'.format(id))
-            return id
-        else:
-            gifts[id] += gifts[next_id]  # Takes next elf's gifts
-            gifts[next_id] = 0
-            playing.remove(next_id)  # No presents? You're out!
-        id = next_elf(next_id, playing, no_of_elves)
+    part_a_elf = part_a(playing, no_of_elves)
+    print('Part A: {} - The winning elf'.format(part_a_elf))
 
 
 if __name__ == '__main__':
