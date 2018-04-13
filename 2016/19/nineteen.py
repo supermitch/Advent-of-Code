4dfdfc66de2bd82c3f0bb00d7d7e50fffd5249dc
Mitch LeBlanc <supermitch@gmail.com>
2018-04-12 21:36:17 -0700

    Major refactor of 2016.19.A.Python
    Use a rotating, popping deque to magically play the game.
    Speed things up 10 times with only 5 lines of code.

--- 2016/19/nineteen.py
+++ 2016/19/nineteen.py
@@ -1,28 +1,20 @@
-def next_elf(id, playing, no_of_elves):
-    while True:
-        id += 1
-        id = id % (no_of_elves + 1)
-        id = max(id, 1)  # Elf indices start at 1
-        if id in playing:  # This elf in the game?
-            return id
+from collections import deque
 
 
-def part_a(playing, no_of_elves):
-    id = 1  # Starts at elf 1
+def game_a(no_of_elves):
+    players = deque(range(1, no_of_elves + 1))
     while True:
-        next_id = next_elf(id, playing, no_of_elves)
-        if next_id == id:
-            return id
-        playing.remove(next_id)  # Next elf loses all presents: they're out
-        id = next_elf(next_id, playing, no_of_elves)
+        players.rotate(-1)
+        players.popleft()
+        if len(players) == 1:
+            return players.popleft()
 
 
 def main():
     no_of_elves = 3012210
-    playing = set(range(1, no_of_elves + 1))
 
-    part_a_elf = part_a(playing, no_of_elves)
-    print('Part A: {} - The winning elf'.format(part_a_elf))
+    winner_a = game_a(no_of_elves)
+    print('Part A: {} - The winning elf'.format(winner_a))
 
 
 if __name__ == '__main__':
