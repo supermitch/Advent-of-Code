8b9cf21accb449a1b8320c17e6a498126bd52087
Mitch LeBlanc <mitch.leblanc@yardi.com>
2018-06-01 11:23:05 -0700

    Refactor 2015.15.Python
    Stop using Shotgun and just check all combinations.

--- 2015/15/fifteen.py
+++ 2015/15/fifteen.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 from collections import namedtuple
-from pprint import pprint
-from random import randint
 
 
 def read_ingredients(f):
@@ -9,7 +7,7 @@ def read_ingredients(f):
     ingredients = []
     for line in f:
         parts = line.replace(':', '').replace(',', '').split()
-        values = [int(parts[i]) for i in (2, 4, 6, 8, 10)]
+        values = [int(parts[x]) for x in (2, 4, 6, 8, 10)]
         ingredients.append(Ingredient(*values))
     return ingredients
 
@@ -19,23 +17,8 @@ def score(recipe):
     dur = max(sum(qty * x.durability for x, qty in recipe), 0)
     flv = max(sum(qty * x.flavor for x, qty in recipe), 0)
     txt = max(sum(qty * x.texture for x, qty in recipe), 0)
-
     cal = max(sum(qty * x.calories for x, qty in recipe), 0)
-
-    return (cap * dur * flv * txt, cal)
-
-
-def random_parts(t=100, k=4):
-    """
-    Produce a partition of t into k values,
-    e.g. t=10, k=4 could yield [1, 1, 3, 5]
-    """
-    rs = [0] + sorted([randint(1, t - 1) for i in range(k - 1)]) + [t]
-    out = []
-    for i in range(len(rs) - 1):
-        out.append(rs[i + 1] - rs[i])
-    assert sum(out) == 100, "Sum of parts was not 100!"
-    return out
+    return (cap * dur * flv * txt, cal)  # Return calories separately
 
 
 def main():
@@ -50,37 +33,20 @@ def main():
     with open('input.txt', 'r') as f:
         sugar, sprinkles, candy, chocolate = read_ingredients(f)
 
-    # Part A
-    max = 0
-    max_recipe = None
-    sums = []
-    for i in range(30000):
-        qtys = random_parts()
-        recipe = [(sugar, qtys[0]), (sprinkles, qtys[1]), (candy, qtys[2]), (chocolate, qtys[3])]
-        total, cals = score(recipe)
-        if total > 0:
-            sums.append((total, qtys))
-            if total > max:
-                max = total
-                max_recipe = recipe
-    pprint(sorted(sums)[-5:])
-    print('Max: ', max_recipe)
-
-    # Part B
-    max = 0
-    max_recipe = None
-    sums = []
-    for i in range(30000):
-        qtys = random_parts()
-        recipe = [(sugar, qtys[0]), (sprinkles, qtys[1]), (candy, qtys[2]), (chocolate, qtys[3])]
-        total, cals = score(recipe)
-        if total > 0 and cals == 500:
-            sums.append((total, qtys))
-            if total > max:
-                max = total
-                max_recipe = recipe
-    pprint(sorted(sums)[-5:])
-    print('Max: ', max_recipe)
+    max_score = 0  # Part A: Best cookie possible
+    max_score_calories = 0  # Part B
+    for q1 in range(0, 101):
+        for q2 in range(0, 101 - q1):
+            for q3 in range(0, 101 - q1 - q2):
+                q4 = 100 - q1 - q2 - q3
+                recipe = [(sugar, q1), (sprinkles, q2), (candy, q3), (chocolate, q4)]
+                total, calories = score(recipe)
+                if total > max_score:
+                    max_score = total
+                if total > max_score_calories and calories == 500:
+                    max_score_calories = total
+    print('Part A: {} - Max score of best cookie possible'.format(max_score))
+    print('Part B: {} - Max score for 500 calorie cookie'.format(max_score_calories))
 
 
 if __name__ == '__main__':
