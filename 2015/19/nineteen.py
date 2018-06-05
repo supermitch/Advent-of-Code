5886b22be33b480b4a670574608748eb0f320c1f
Mitch LeBlanc <supermitch@gmail.com>
2018-06-04 22:44:46 -0700

    2015.19.Python WIP

--- 2015/19/nineteen.py
+++ 2015/19/nineteen.py
@@ -16,12 +16,38 @@ def read_input():
 
 
 def find_all(replacements, start):
+    options = []
+    for sub, elements in replacements.items():
+        for element in elements:
+            idx = 0
+            while True:
+                idx = start.find(sub, idx)
+                if idx != -1:  # If found
+                    options.append(start[:idx] + element + start[idx + len(sub):])
+                    idx += 1
+                else:
+                    break
     return options
 
 
 def find_distinct(replacements, start):
     options = find_all(replacements, start)
     return set(options)
+
+
+def fabricate_molecule(replacements, molecule):
+    options = ['e']  # Starting molecule
+    count = 0
+    while True:
+        count += 1
+        print(count)
+        new_options = set()
+        for option in options:
+            new = find_all(replacements, option)
+            if molecule in new:
+                return count
+            new_options.update(new)
+        options = new_options.copy()
 
 
 def main():
@@ -32,8 +58,15 @@ def main():
     assert len(find_distinct(test_replacements, 'HOHOHO')) == 7
 
     replacements, molecule = read_input()
-    print(replacements)
-    print(molecule)
+    distinct = find_distinct(replacements, molecule)
+    print('Part A: {} - Distinct molecules after all single replacements'.format(len(distinct)))
+
+    test_replacements = {'e': ['H', 'O'], 'H': ['HO', 'OH'], 'O': ['HH']}
+    assert fabricate_molecule(test_replacements, 'HOH') == 3
+    assert fabricate_molecule(test_replacements, 'HOHOHO') == 6
+
+    count = fabricate_molecule(replacements, molecule)
+    print('Part B: {} - Steps until medicine is fabricated'.format(count))
 
 
 if __name__ == '__main__':
