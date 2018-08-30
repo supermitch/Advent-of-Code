9492c504fb4db58ca74458bdc05d459a6151f2e6
Mitch LeBlanc <supermitch@gmail.com>
2018-08-29 21:13:38 -0700

    Minor refactor 2015.25

--- 2015/25/twenty_five.py
+++ 2015/25/twenty_five.py
@@ -7,16 +7,15 @@ def calculate(init, row, col):
     for d in itertools.count(start=1):  # Infinite series of diagonals
         for k in range(0, d + 1):
             val = init * 252533 % 33554393
-            if d - k == 3009 and k == 3018:  # row = d-k, col = k
+            if d - k == row and k == col:
                 return val
             init = val
 
 
 def main():
     init = 20151125
-    row = 3010
-    col = 3019
-    value = calculate(init, row, col)
+    row, col = (3010, 3019)
+    value = calculate(init, row - 1, col - 1)
     print('Part A: {} - Value at row {} and col {}'.format(value, row, col))
 
 
