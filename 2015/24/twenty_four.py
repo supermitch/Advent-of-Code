8b8968586ef730500b2f15633cb0eb41e1cf794d
Mitch LeBlanc <mleblanc@zenefits.com>
2018-09-30 09:40:32 -0700

    Horrible solution to 2015.24 in Python
    Extremely slow, but actually worked.

--- 2015/24/twenty_four.py
+++ 2015/24/twenty_four.py
@@ -36,8 +36,6 @@ def find_arrangements(weights):
                     if sum(g2) != target:
                         continue
 
-                    g3 = g2_weights - set(g2)  # Group 3 order is unimportant
-
                     if found_g1_size is None:
                         found_g1_size = n1
                     elif n1 > found_g1_size:  # We already found all the smallest group 1s
@@ -52,9 +50,66 @@ def find_arrangements(weights):
                         if entanglement < lowest_entanglement:
                             lowest_entanglement = entanglement
                     else:
+                        g3 = g2_weights - set(g2)  # Group 3 order is unimportant
                         entanglement = product(g3)
                         if entanglement < lowest_entanglement:
                             lowest_entanglement = entanglement
+
+
+def find_arrangements_four(weights):
+    target = sum(weights) / 4
+    n = len(weights)
+    lowest_entanglement = 999999999999999
+    best_arrangement = None
+    seen = set()
+    found_g1_size = None
+    for n1 in range(1, n - 2):
+        for n2 in range(1, n - n1 - 1):
+            for n3 in range(1, n - n1 - n2 - 1):
+                n4 = n - n1 - n2 - n3
+                print('n: {}, {}, {}, {}'.format(n1, n2, n3, n4))
+
+                sizes = tuple(sorted([n1, n2, n3, n4]))
+                if sizes in seen:  # No repeat sizes, e.g. 2,2,4 = 2,4,2 = 4,2,2
+                    continue
+                seen.add(sizes)
+
+                for g1 in combinations(weights, n1):
+                    if sum(g1) != target:
+                        continue
+
+                    g2_weights = weights - set(g1)
+                    for g2 in combinations(g2_weights, n2):
+                        if sum(g2) != target:
+                            continue
+
+                        g3_weights = g2_weights - set(g2)
+                        for g3 in combinations(g3_weights, n3):
+                            if sum(g3) != target:
+                                continue
+
+                            if found_g1_size is None:
+                                found_g1_size = n1
+                            elif n1 > found_g1_size:  # We already found all the smallest group 1s
+                                return lowest_entanglement
+
+                            if n1 < n2 and n1 < n3 and n1 < n4:
+                                entanglement = product(g1)
+                                if entanglement < lowest_entanglement:
+                                    lowest_entanglement = entanglement
+                            elif n2 < n3 and n2 < n4:
+                                entanglement = product(g2)
+                                if entanglement < lowest_entanglement:
+                                    lowest_entanglement = entanglement
+                            elif n3 < n4:
+                                entanglement = product(g3)
+                                if entanglement < lowest_entanglement:
+                                    lowest_entanglement = entanglement
+                            else:
+                                g4 = g3_weights - set(g3)
+                                entanglement = product(g4)
+                                if entanglement < lowest_entanglement:
+                                    lowest_entanglement = entanglement
 
 
 def main():
@@ -68,6 +123,8 @@ def main():
     lowest_entanglement = find_arrangements(weights)
     print(lowest_entanglement)
 
+    lowest_entanglement = find_arrangements_four(weights)
+    print(lowest_entanglement)
 
 if __name__ == '__main__':
     main()
