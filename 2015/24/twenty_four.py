3060d2c629505b85e084e0d2da55dfb005b9912e
Mitch LeBlanc <mleblanc@zenefits.com>
2018-09-29 23:33:19 -0700

    Add 2015.24.A in Python

--- 2015/24/twenty_four.py
+++ 2015/24/twenty_four.py
@@ -10,9 +10,11 @@ def product(iter):
     return acc
 
 
-def find_arrangements(weights, n_groups=3):
+def find_arrangements(weights):
+    target = sum(weights) / 3
     n = len(weights)
-    arrangements = []
+    lowest_entanglement = 999999999999999
+    best_arrangement = None
     seen = set()
     found_g1_size = None
     for n1 in range(1, n - 2):
@@ -23,50 +25,48 @@ def find_arrangements(weights, n_groups=3):
             sizes = tuple(sorted([n1, n2, n3]))
             if sizes in seen:  # No repeat sizes, e.g. 2,2,4 = 2,4,2 = 4,2,2
                 continue
-            else:
-                seen.add(sizes)
+            seen.add(sizes)
 
             for g1 in combinations(weights, n1):
-                s1 = sum(g1)
+                if sum(g1) != target:
+                    continue
+
                 g2_weights = weights - set(g1)
                 for g2 in combinations(g2_weights, n2):
-                    s2 = sum(g2)
-                    if s1 != s2:
+                    if sum(g2) != target:
                         continue
-                    p1 = product(g1)
-                    p2 = product(g2)
+
                     g3 = g2_weights - set(g2)  # Group 3 order is unimportant
-                    if s2 == sum(g3):  # We already know g1 matches g2
-                        if found_g1_size is None:
-                            found_g1_size = n1
-                        elif n1 > found_g1_size:  # We already found all the smallest group 1s
-                            return arrangements
-                        if n1 < n2 and n1 < n3:
-                            entanglement = p1
-                            # arrangements.append((n1, entanglement, g1, g2, g3))
-                            arrangements.append((n1, g1))
-                        elif n2 < n3:
-                            entanglement = p2
-                            # arrangements.append((n2, entanglement, g2, g2, g3))
-                            arrangements.append((n2, g2))
-                        else:
-                            entanglement = product(g3)
-                            # arrangements.append((n3, entanglement, g2, g2, g3))
-                            arrangements.append((n3, g3))
-    return arrangements
+
+                    if found_g1_size is None:
+                        found_g1_size = n1
+                    elif n1 > found_g1_size:  # We already found all the smallest group 1s
+                        return lowest_entanglement
+
+                    if n1 < n2 and n1 < n3:
+                        entanglement = product(g1)
+                        if entanglement < lowest_entanglement:
+                            lowest_entanglement = entanglement
+                    elif n2 < n3:
+                        entanglement = product(g2)
+                        if entanglement < lowest_entanglement:
+                            lowest_entanglement = entanglement
+                    else:
+                        entanglement = product(g3)
+                        if entanglement < lowest_entanglement:
+                            lowest_entanglement = entanglement
 
 
 def main():
     test_weights = {1, 2, 3, 4, 5, 7, 8, 9, 10, 11}
-    # arrangements = find_arrangements(test_weights)
+    lowest_entanglement = find_arrangements(test_weights)
+    print(lowest_entanglement)
 
     with open('input.txt', 'r') as f:
         weights = {int(l.strip()) for l in f}
 
-    print('Found {} weights'.format(len(weights)))
-
-    for groups in find_arrangements(weights):
-        print(groups)
+    lowest_entanglement = find_arrangements(weights)
+    print(lowest_entanglement)
 
 
 if __name__ == '__main__':
