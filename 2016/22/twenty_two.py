062bd96e84547ed690bd94e1e28a7ad998345615
Mitch LeBlanc <supermitch@gmail.com>
2018-11-14 20:39:29 -0800

    Resolve merge conflicts

--- 2016/22/twenty_two.py
+++ 2016/22/twenty_two.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from itertools import groupby
 import re
 
@@ -11,15 +12,10 @@ class Disk:
         self.avail = total - used
 
     def __repr__(self):
-        return ('Disk({}, {}, {}/{}: {})'
-                ''.format(self.x, self.y, self.used, self.total, self.avail))
-
+        return 'Disk({}-{}={})'.format(self.total, self.used, self.avail)
 
 def parse(line):
-    match = re.match(
-        r'^.*\-x([0-9]+)\-y([0-9]+) +([0-9]+)T +([0-9]+)T.*$',
-        line
-    )
+    match = re.match(r'.*-x([0-9]+)\-y([0-9]+) +([0-9]+)T +([0-9]+).*', line)
     if match:
         return Disk(*[int(x) for x in match.groups()])
 
@@ -35,22 +31,31 @@ def main():
     with open('input.txt', 'r') as f:
         data = [parse(l.strip()) for l in f if 'node' in l]
 
-    use_groups = group_by(data, 'used')
+    used_groups = group_by(data, 'used')
     avail_groups = group_by(data, 'avail')
 
     pairs_count = 0
-    for used in use_groups.keys():
+    for used in used_groups.keys():
         if used == 0:  # Skip this one
             continue
         for avail in avail_groups.keys():
             if used <= avail:
-                pairs_count += len(use_groups[used]) * len(avail_groups[avail])
+                pairs_count += len(used_groups[used]) * len(avail_groups[avail])
     print('Part A: {} - Number of available pairs'.format(pairs_count))
 
+    from pprint import pprint
+    grid = {}
+    for disk in data:
+        grid[(disk.x, disk.y)] = disk
 
-    grid = [[] 
+    for y in range(30):
+        print(''.join(str(grid[(x,y)]) for x in range(34)))
+
+    grid = [[]]
 
     for disk in data:
         grid[disc.x][disc.y] = disk
+
+
 if __name__ == '__main__':
     main()
