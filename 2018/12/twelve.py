a3dad2fdbfa4e760128d2c6a17580c8d8a06403b
Mitch LeBlanc <mleblanc@zenefits.com>
2018-12-13 22:42:29 -0800

    2018.12 Part B and refactor

--- 2018/12/twelve.py
+++ 2018/12/twelve.py
@@ -1,68 +1,27 @@
 #!/usr/bin/env python
 
-rules = {
-    '#.##.': '.',
-    '#.#..': '.',
-    '###.#': '.',
-    '..#.#': '.',
-    '....#': '.',
-    '.####': '.',
-    '##.##': '#',
-    '###..': '#',
-    '.###.': '#',
-    '...#.': '.',
-    '.....': '.',
-    '##..#': '.',
-    '.#.#.': '#',
-    '.#.##': '#',
-    '##.#.': '.',
-    '##...': '.',
-    '#####': '#',
-    '#...#': '.',
-    '..##.': '.',
-    '..###': '.',
-    '.#...': '#',
-    '.##.#': '.',
-    '#....': '.',
-    '.#..#': '.',
-    '.##..': '#',
-    '...##': '#',
-    '#.###': '.',
-    '#..#.': '.',
-    '..#..': '#',
-    '#.#.#': '#',
-    '####.': '#',
-    '#..##': '.',
-}
+def read_input():
+    with open('input.txt') as f:
+        pots = f.readline().strip().split(':')[1].strip()
+        f.readline()
+        rules = {}
+        for line in f:
+            k, v = line.strip().split(' => ')
+            rules[k] = v
+        return pots, rules
 
-ruless = {
-'...##': '#',
-'..#..': '#',
-'.#...': '#',
-'.#.#.': '#',
-'.#.##': '#',
-'.##..': '#',
-'.####': '#',
-'#.#.#': '#',
-'#.###': '#',
-'##.#.': '#',
-'##.##': '#',
-'###..': '#',
-'###.#': '#',
-'####.': '#',
 
-}
 def main():
     print('\ntwelve')
-    init = '..#..####.##.####...#....#######..#.#..#..#.#.#####.######..#.#.#.#..##.###.#....####.#.#....#.#####'
-    # init = '#..#.#..##......###...###'
-    add = 25
-    pots = ['.'] * add + [x for x in init] + ['.'] * add
+    init, rules = read_input()
+
+    left = 5
+    pots = ['.'] * left + [x for x in init] + ['.'] * 20
     g = 0
-    gens = 50000000000
+    gens = 102
     while g < gens:
-        if not (g % 100):
-            print(g)
+        print(g, ''.join(pots))
+        print(pots.index('#'))
         nxt = ['.' for x in pots]
         for i in range(0, len(pots) - 4):
             span = ''.join(pots[i:i + 5])
@@ -72,18 +31,27 @@ def main():
                 nxt[i+2] = '.'
         g += 1
         pots = nxt[:]
-        if any(x == '#' for x in pots[:5]) or any(x == '#' for x in pots[-5:]):
+        if any(x == '#' for x in pots[:5]):
             pots = ['.'] * 5 + pots
+            left += 5
+        if any(x == '#' for x in pots[-5:]):
             pots = pots + ['.'] * 5
-            add += 5
+        if g == 20:
+            break
+        if g == 102:
+            final = pots[:]
+            break
 
-    print(''.join(pots))
-    total = [i - add if x == '#' else 0 for i, x in enumerate(pots[add:], start=add)]
-    sub = [i - add for i, x in enumerate(pots[:add + 1]) if x == '#']
-    print(total, sum(total))
-    print(sub, sum(sub))
+    # print(final.index('#') - 5)
+    delta = 50000000000 - 102
+
+    total = [i - left if x == '#' else 0 for i, x in enumerate(pots[left:], start=left)]
+    sub = [i - left for i, x in enumerate(pots[:left + 1]) if x == '#']
+    print(total, sub)
     print(sum(total) + sum(sub))
-    # 758 wrong
+
+    # 6906 too low
+    # 7090
 
 if __name__ == '__main__':
     main()
