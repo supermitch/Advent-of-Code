3e24c69067a3b404c1106fe57727ad4aa590d53e
Mitch LeBlanc <mitch.leblanc@docusign.com>
2019-11-11 22:01:01 -0800

    Complete 2018.20 in Python

--- 2018/20/twenty.py
+++ 2018/20/twenty.py
@@ -63,36 +63,63 @@ def extract_path(data):
             left -= 1
             if left == 0:
                 new_data = ''.join(new)
-                print('new data', new_data, data[len(new_data) + 2:])
                 return ''.join(new), data[len(new_data) + 2:]
         new.append(c)
-    raise ValueError('Did not found matching bracket')
+
+
+def cheapest(costs, unvisited):
+    for coord, _ in sorted(costs.items(), key=lambda x: x[1]):
+        if coord in unvisited:
+            return coord
+
+
+def dijkstras(grid):
+    x, y = (0, 0)
+    costs = {k: float('Inf') for k, v in grid.items() if v in 'X.'}
+    unvisited = set([k for k, v in grid.items() if v in 'X.'])
+    coord = (x, y)
+    costs[coord] = 0
+    route = {coord: None}
+    while True:
+        current_cost = costs[coord]
+        x, y = coord
+        for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            door = (x + dx, y + dy)
+            if door in grid and grid[door] in '|-':
+                nxt = (x + dx * 2, y + dy * 2)
+                if nxt in unvisited:
+                    tentative_cost = current_cost + 1
+                    if tentative_cost < costs[nxt]:
+                        costs[nxt] = tentative_cost
+                        route[nxt] = coord
+        unvisited.remove(coord)
+        if not unvisited:
+            return costs
+        coord = cheapest(costs, unvisited)
 
 
 def main():
     with open('input.txt') as f:
         data = f.readline().strip()[1:-1]
-
-    data = 'ENWWW(NEEE|SSE(EE|N)|W)'
-    # data = 'ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN'
-    coord = (0, 0)
-    grid = {coord: 'X'}
-
+    grid = {(0, 0): 'X'}
     graph = [((0,0), data)]
     while graph:
         coord, data = graph.pop()
         while data:
-            print(coord, data)
-            draw(grid)
             step = data[0]
             if step == '(':
                 leg, data = extract_path(data)
                 for option in extract_options(leg):
                     graph.append((coord, option))
-                print('graph:', graph)
             else:
                 coord, grid = add_map_step(coord, grid, step)
                 data = data[1:]
+    costs = dijkstras(grid)
+    costs = sorted(costs.items(), key=lambda x: x[1])
+    longest = costs[-1][1]
+    print(f'Part A: {longest} - Longest shortest path')
+    rooms = sum(1 for k, v in costs if v >= 1000)
+    print(f'Part B: {rooms} - Rooms over 1000 doors from start')
 
 
 if __name__ == '__main__':
