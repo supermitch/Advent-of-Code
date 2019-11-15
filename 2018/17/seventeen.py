fbb413374b14cc6820f8f1b0e5df0cb0ca7e3178
Mitch LeBlanc <mitch.leblanc@docusign.com>
2019-11-14 23:03:48 -0800

    Add 2018.17 in Python

--- 2018/17/seventeen.py
+++ 2018/17/seventeen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import re
 
+
 def parse(l):
     a, b, c = [int(x) for x in re.sub('[^0-9]', ' ', l).split()]
     if l[0] == 'x':
@@ -9,89 +10,132 @@ def parse(l):
         span = [(x, a) for x in range(b, c + 1)]
     return span
 
-def is_bounded(grid, x, y):
-    if grid[y + 1][x] != '#':
-        return False
-    while True:
-        x += 1
-        if grid[y][x] == '#':
-            break
-        else:
-            if grid[y + 1][x] != '#':
-                return False
-    while True:
-        x -= 1
-        if grid[y][x] == '#':
-            break
-        else:
-            if grid[y + 1][x] != '#':
-                return False
 
-def draw(water, grid, xmin):
-    for y, row in enumerate(grid):
+def find_limits(data):
+    xmin, xmax, ymin, ymax = float('inf'), 0, float('inf'), 0
+    for span in data:
+        for x, y in span:
+            xmin = min(x, xmin)
+            xmax = max(x, xmax)
+            ymax = max(y, ymax)
+            ymin = min(y, ymin)
+    return xmin, xmax, ymin, ymax
+
+
+def generate_grid(data, xmax, ymax):
+    grid = [[' ' for x in range(xmax + 1)] for y in range(ymax+ 1)]
+    for span in data:
+        for x, y in span:
+            grid[y][x] = '#'
+    grid[0][500] = '+'  # Original source
+    return grid
+
+
+def is_bounded(grid, xi, y):
+    """ Is a floor bounded on both sides by walls? Return bounded coords. """
+    assert grid[y + 1][xi] in '~#'
+    coords = []
+    x = xi
+    while grid[y + 1][x] in '~#' and grid[y][x] not in '~#':
+        coords.append((x, y))
+        x += 1
+    if grid[y][x] not in '~#':
+        return False
+    x = xi
+    while grid[y + 1][x] in '~#' and grid[y][x] not in '~#':
+        coords.append((x, y))
+        x -= 1
+    if grid[y][x] not in '~#':
+        return False
+    for x, y in coords:
+        grid[y][x] = '~'
+    return True
+
+
+def flow_grid(grid, xi, y):
+    x = xi
+    new_drops = []
+    while grid[y + 1][x] in '~#' and grid[y][x] not in '~#':
+        grid[y][x] = '|'
+        x += 1
+    if grid[y][x] not in '~#':
+        grid[y][x] = '|'
+        new_drops.append((x, y))
+    x = xi
+    while grid[y + 1][x] in '~#' and grid[y][x] not in '~#':
+        grid[y][x] = '|'
+        x -= 1
+    if grid[y][x] not in '~#':
+        grid[y][x] = '|'
+        new_drops.append((x, y))
+    return new_drops
+
+
+def draw(grid, xmin):
+    string_grid = ''
+    for row in grid:
         line = ''
-        for x, col in enumerate(row[xmin - 5:], start=xmin - 5):
-            flow = water[y][x]
-            line += flow if flow else col
-        print(line)
+        for col in row[xmin - 5:]:
+            line += col
+        string_grid += line + '\n'
+    print(string_grid)
+
 
 class Drop:
-    def __init__(self, x, y):
-        self.x, self.y = x, y
-        self.settle = False
+    def __init__(self, x, y, grid):
+        self.x = x
+        self.y = y
+        self.grid = grid
 
-    def update(self, grid, water):
-        # try to move down
-        nx, ny = self.x, self.y + 1
-            if grid[ny][nx] in ('.', '|'):
-                self.x = nx, self.y = ny
-                grid[ny][nx] == '|'
-                continue
-            elif grid[ny][nx] in ('#', '~'):
-                while True:
-                    nx, ny = self.x + 1, self.y
-                    if grid[ny][nx] in ('.', '|'):
-                        grid[ny][nx] = '|'
-                        self.x, self.y = nx, ny
-                    elif grid[ny][nx] in ('#', '~'):
+    def flow(self):
+        while True:
+            try:
+                if self.grid[self.y + 1][self.x] in '~#':
+                    break
+                elif self.grid[self.y + 1][self.x] in '|':
+                    return []
+            except IndexError:
+                return []
+            self.y += 1
+            self.grid[self.y][self.x] = '|'
+        if is_bounded(self.grid, self.x, self.y):
+            return [(self.x, self.y - 1)]
+        else:
+            return flow_grid(self.grid, self.x, self.y)
+
+
+def count_water(grid, ymin, ymax):
+    flow = 0
+    flood = 0
+    for row in grid[ymin:ymax + 1]:
+        for cell in row:
+            if cell == '|':
+                flow += 1
+            elif cell in '~':
+                flood += 1
+    return flow, flood
 
 
 def main():
     with open('input.txt') as f:
         data = [parse(l.strip()) for l in f]
-    print(data[0])
 
-    xmax = 0
-    xmin = 99999
-    ymax = 0
-    ymin = 99999
-    for span in data:
-        for x, y in span:
-            if x < xmin:
-                xmin = x
-            if x > xmax:
-                xmax = x
-            if y > ymax:
-                ymax = y
-            if y < ymin:
-                ymin = y
-    print('Limits:', xmin, xmax, ymin, ymax)
+    _, xmax, ymin, ymax = find_limits(data)
 
-    grid = [['.' for x in range(xmax + 1)] for y in range(ymax + 1)]
-    grid[0][500] = '+'
-    water = [[None for x in range(xmax + 1)] for y in range(ymax + 1)]
+    grid = generate_grid(data, xmax, ymax)
 
-    for span in data:
-        for x, y in span:
-            grid[y][x] = '#'
+    drops = [Drop(500, 0, grid)]
+    while drops:
+        new_drops = []
+        for drop in drops:
+            res = drop.flow()
+            new_drops.extend([Drop(d[0], d[1], grid) for d in res])
+        drops = new_drops
 
+    flow, flood = count_water(grid, ymin, ymax)
+    print(f'Part A: {flow + flood} - Total water in sand')
+    print(f'Part B: {flood} - Retained water after spring dries up')
 
-    draw(water, grid, xmin)
-
-    while True:
-        drop = Drop(500, 0)
-        while not drop.settle:
-            drop.update(grid)
 
 if __name__ == '__main__':
     main()
