c0a7a1056943617f56b741013948b01b9d0a27ce
Mitch LeBlanc <mitch.leblanc@docusign.com>
2019-11-15 22:58:53 -0800

    Add 2018.24 in Python

--- 2018/24/twenty_four.py
+++ 2018/24/twenty_four.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
-def parse_army(f, side, lines=10, offset=0):
+class Group:
+    def __init__(
+        self, side, id, units, hp, immunes, weaks, attack, kind, initiative
+        ):
+        self.side = side
+        self.id = id
+        self.units = units
+        self.hp = hp
+        self.immunes = immunes
+        self.weaks = weaks
+        self.attack = attack
+        self.kind = kind
+        self.initiative = initiative
+        self.attacker = None
+        self.target = None
+        self.damage = None
+
+    def __str__(self):
+        return f'{self.side}-{self.id}: {self.units} units, {self.hp} hp)'
+
+    @property
+    def power(self):
+        return self.units * self.attack
+
+def parse_army(f, side, lines, offset=0):
     army = []
     for g in range(lines):
         line = f.readline().replace('(', ' ').split()
@@ -15,23 +39,29 @@ def parse_army(f, side, lines=10, offset=0):
                 weaks.append(line[i].replace(')', '').replace(';', '').replace(',', ''))
                 if ';' in line[i] or ')' in line[i]:
                     break
-        army.append({
-            'side': side,
-            'id': offset + g + 1,
-            'units': int(line[0]),
-            'hp': int(line[4]),
-            'immunes': immunes,
-            'weaks': weaks,
-            'damage': int(line[line.index('does') + 1]),
-            'kind': line[line.index('does') + 2],
-            'initiative': int(line[-1]),
-        })
+        army.append(Group(
+            side,
+            offset + g + 1,
+            int(line[0]),
+            int(line[4]),
+            immunes,
+            weaks,
+            int(line[line.index('does') + 1]),
+            line[line.index('does') + 2],
+            int(line[-1]),
+        ))
     return army
 
-def read_input():
-    fname = 'test.txt'
-    lines = 2
-    with open(fname) as f:
+
+def read_input(path):
+    lines = -2
+    with open(path) as f:
+        for line in f:
+            lines += 1
+            if not line.strip():
+                break
+
+    with open(path) as f:
         f.readline()  # Header
         immune = parse_army(f, 'immune', lines=lines)
         f.readline()
@@ -39,97 +69,97 @@ def read_input():
         infection = parse_army(f, 'infection', lines=lines, offset=lines)
     return immune, infection
 
-def set_attacker(groups, target, attacker):
-    for group in groups:
-        if group['id'] == target:
-            group['attacker'] == attacker
-            return
 
 def find_target(groups, target):
     for group in groups:
-        if group['id'] == target:
+        if group.id == target:
             return group
 
+
 def calc_damage(defender, attacker):
-    attacker['power'] = attacker['units'] * attacker['damage']
-    if attacker['kind'] in defender['immunes']:
-        print('immune!')
+    if attacker.kind in defender.immunes:
         return 0
-    elif attacker['kind'] in defender['weaks']:
-        print('weakness!')
-        return attacker['power'] * 2
+    elif attacker.kind in defender.weaks:
+        return attacker.power * 2
     else:
-        print('regular')
-        return attacker['power']
+        return attacker.power
 
-def army_won(groups):
-    i = set([g['side'] for g in groups])
 
-def main():
-    immune, infection = read_input()
+def boost(groups, value):
+    for g in groups:
+        g.attack += value
+
+
+def fight_battle(path, boost_value=0):
+    immune, infection = read_input(path)
+    boost(immune, boost_value)  # Apply attack boost to immune groups
     groups = immune + infection
-    i = 0
-    game_over = False
+
     while True:
-        if game_over:
-            break
-        i += 1
+        groups = [g for g in groups if g.units > 0]
 
-        groups = [g for g in groups if g['units'] >= 0]
-
-        print(i, len(groups))
         # Select phase
         for group in groups:
-            group['power'] = group['units'] * group['damage']
-            group['target'] = None
-            group['attacker'] = None
-            group['dmg'] = None
-        groups = sorted(groups, key=lambda x: x['initiative'], reverse=True)
-        groups = sorted(groups, key=lambda x: x['power'], reverse=True)
+            group.target = None
+            group.attacker = None
+            group.damage = None
+        groups = sorted(groups, key=lambda x: x.initiative, reverse=True)
+        groups = sorted(groups, key=lambda x: x.power, reverse=True)
 
-        for curr in groups:
-            targets = [g for g in groups if g['side'] != curr['side'] and g['attacker'] is None]
-            for group in targets:
-                if group['attacker'] is None:  # Not spoken for
-                    group['dmg'] = calc_damage(group, curr)
-            targets = sorted(targets, key=lambda x: x['initiative'], reverse=True)
-            targets = sorted(targets, key=lambda x: x['power'], reverse=True)
-            targets = sorted(targets, key=lambda x: x['dmg'], reverse=True)
-            targets = [t for t in targets if t['dmg'] > 0]
-            print('curr', curr['side'], curr['id'])
-            print([(t['side'], t['id'], t['dmg']) for t in targets])
+        for attacker in groups:
+            targets = [g for g in groups if g.side != attacker.side and g.attacker is None]
+            for defender in targets:
+                if defender.attacker is None:  # Not spoken for
+                    defender.damage = calc_damage(defender, attacker)
+            targets = sorted(targets, key=lambda x: x.initiative, reverse=True)
+            targets = sorted(targets, key=lambda x: x.power, reverse=True)
+            targets = sorted(targets, key=lambda x: x.damage, reverse=True)
+            targets = [t for t in targets if t.damage > 0]
+            #print('attacker', attacker.side, attacker.id)
+            #print([(t.side, t.id, t.damage) for t in targets])
             if targets:
                 target = targets[0]
-                curr['target'] = target['id']
-                set_attacker(groups, target['id'], curr['id'])
+                attacker.target = target.id
+                target.attacker = attacker.id
 
         # Attack phase
-        groups = sorted(groups, key=lambda x: x['initiative'], reverse=True)
-        for group in groups:
-            if group['units'] <= 0:  # Can't attack
+        total_loss = 0
+        groups = sorted(groups, key=lambda x: x.initiative, reverse=True)
+        for attacker in groups:
+            if attacker.units <= 0:  # Can't attack
                 continue
-            print('attacker:', group)
-            target = find_target(groups, group['target'])
+            #print('Attacker:', attacker)
+            target = find_target(groups, attacker.target)
             if target:
-                print('target:', target)
-                damage = calc_damage(target, group)
-                loss = damage // target['hp']
-                target['units'] -= loss
-                print('damage:', damage, 'loss:', loss)
-            sides = set([g['side'] for g in groups])
-            print(sides)
+                #print('Target:', target)
+                damage = calc_damage(target, attacker)
+                #print(f'Attacker units: {attacker.units}')
+                loss = min(damage // target.hp, target.units)
+                #print(f'Damage: {damage} target.hp: {target.hp} Loss: {loss}')
+                target.units -= loss
+                total_loss += loss
+            sides = set([g.side for g in groups])
             if len(sides) == 1:
                 winner = sides.pop()
-                army = sum(g['units'] for g in groups \
-                        if g['units'] >= 0 and g['side'] == winner)
-                print(army)
-                game_over = True
-                break
+                army = sum(g.units for g in groups \
+                        if g.units >= 0 and g.side == winner)
+                return army, winner == 'immune'
+        if total_loss <= 0:
+            return None, False  # Deadlocked battlw
 
 
-
-
-
+def main():
+    # Part A b
+    units, healthy = fight_battle('input.txt')
+    print(f'Part A {units} - Number of winning units remaining after battle')
+    # Part B is easy but the battle deadlocks.
+    boost = 0
+    while True:
+        units, healthy = fight_battle('input.txt', boost)
+        if healthy:
+            break
+        boost += 1
+    print(f'Part B {boost} - Boost required for immune system to win')
 
 
 if __name__ == '__main__':
