fd9b057ed664e115b59a73cac4835ec7bc4a1102
Mitch LeBlanc <mitch.leblanc@docusign.com>
2019-12-04 18:19:25 -0800

    Refactor 2019.04 in Python

--- 2019/04/four.py
+++ 2019/04/four.py
@@ -3,28 +3,22 @@ import itertools
 import operator
 
 
-def adj(string, strict=False):
+def adj(i, strict=False):
     func = operator.eq if strict else operator.ge
-    return any(func(len(list(g)), 2) for _, g in itertools.groupby(string))
+    return any(func(len(list(g)), 2) for _, g in itertools.groupby(str(i)))
 
 
-def sorter(string):
-    return ''.join(sorted(string)) == string
+def sorter(i):
+    return ''.join(sorted(str(i))) == str(i)
 
 
 def main():
-    count = 0
-    strict = 0
-    for i in range(156218, 652527 + 1):
-        string = str(i)
-        if sorter(string):
-            if adj(string):
-                count += 1
-            if adj(string, strict=True):
-                strict += 1
+    a, b = 156218, 652527
+    part_a = sum(sorter(i) and adj(i) for i in range(a, b + 1))
+    part_b = sum(sorter(i) and adj(i, strict=True) for i in range(a, b + 1))
 
-    print(f'Part A: {count} - No. of potential passwords')
-    print(f'Part B: {strict} - No. of potential passwords with strict batches')
+    print(f'Part A: {part_a} - No. of potential passwords')
+    print(f'Part B: {part_b} - No. of potential passwords with strict batches')
 
 
 if __name__ == '__main__':
