ea29fdb96251d67cb8f162f91920891958aeff5f
Mitch LeBlanc <supermitch@gmail.com>
2019-12-05 21:39:12 -0800

    Refactor 2019.06 in Python

--- 2019/06/six.py
+++ 2019/06/six.py
@@ -1,46 +1,33 @@
 #!/usr/bin/env python
 
 
-def parse(l):
-    return [x for x in l.split(')')]
-
-
 def main():
     with open('input.txt') as f:
-        data = [parse(l.strip()) for l in f]
+        data = [l.strip().split(')') for l in f]
 
-    graph = {}
-    for a, b in data:
-        graph[b] = a
+    orbits = {b: a for a, b in data}
 
     total = 0
-    for k in graph.keys():
+    for k in orbits.keys():
         path = [k]
-        nxt = k
         while True:
-            nxt = graph[nxt]
-            path.append(nxt)
-            if nxt == 'COM':
+            k = orbits[k]
+            if k == 'COM':  # Don't include COM
                 break
-        total += len(path) - 1
+            path.append(k)
+        total += len(path)
         if path[0] == 'YOU':
             you = path.copy()
         elif path[0] == 'SAN':
             san = path.copy()
     print(f'Part A: {total} - Total no. of direct & indirect orbits')
 
-    count = 0
-    for o in you:
-        count += 1
-        if o in san:
+    for obj in you:
+        if obj in san:
             break
-    steps = 0
-    for x in san:
-        steps += 1
-        if x == o:
-            break
-    transers = count + steps - 4
-    print(f'Part B: {transers} - Minimum orbital transfers to reach SAN')
+
+    transfers = you.index(obj) + san.index(obj) - 2  # Ignore YOU & SAN
+    print(f'Part B: {transfers} - Minimum orbital transfers to reach SAN')
 
 
 if __name__ == '__main__':
