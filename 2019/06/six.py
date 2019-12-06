e028c37f0bfe3544b4d01d09e341a0b7bc925bce
Mitch LeBlanc <supermitch@gmail.com>
2019-12-06 08:32:56 -0800

    Refactor 2019.06 in Python

--- 2019/06/six.py
+++ 2019/06/six.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python
+from collections import defaultdict
 
 
 def main():
     with open('input.txt') as f:
         data = [l.strip().split(')') for l in f]
-
     orbits = {b: a for a, b in data}
 
-    total = 0
-    for k in orbits.keys():
-        path = [k]
-        while True:
+    paths = defaultdict(list)
+    for o in orbits.keys():
+        k = o
+        while k != 'COM':
+            paths[o].append(k)
             k = orbits[k]
-            if k == 'COM':  # Don't include COM
-                break
-            path.append(k)
-        total += len(path)
-        if path[0] == 'YOU':
-            you = path.copy()
-        elif path[0] == 'SAN':
-            san = path.copy()
+    total = sum(len(path) for path in paths.values())
     print(f'Part A: {total} - Total no. of direct & indirect orbits')
 
-    for obj in you:
-        if obj in san:
+    for obj in paths['YOU']:
+        if obj in paths['SAN']:
             break
-
-    transfers = you.index(obj) + san.index(obj) - 2  # Ignore YOU & SAN
+    transfers = paths['YOU'].index(obj) + paths['SAN'].index(obj) - 2
     print(f'Part B: {transfers} - Minimum orbital transfers to reach SAN')
 
 
