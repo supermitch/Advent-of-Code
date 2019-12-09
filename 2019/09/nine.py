e01cf9a38d741af6a41dbec8ab4aee59b6fe801f
Mitch LeBlanc <supermitch@gmail.com>
2019-12-08 22:47:01 -0800

    Refactor 2019.09 in Python

--- 2019/09/nine.py
+++ 2019/09/nine.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 def parse_opcode(param):
     param = str(param).zfill(5)  # 0 padded
     op = int(param[-2:])
@@ -13,23 +14,19 @@ def extend(data, n):
 
 
 def get_val(data, x, rel, mode):
-    if mode == 0:
-        if x >= len(data):
-            extend(data, x)
-        return data[x]
-    elif mode == 1:
+    if mode == 1:  # Immediate
         return x
-    elif mode == 2:
-        if (x + rel) >= len(data):
-            extend(data, x + rel)
-        return data[x + rel]
+    if mode == 2:  # Relative
+        x += rel
+    extend(data, x)
+    return data[x]  # Position or Relative modes
 
 
 def set_val(data, value, x, rel, mode):
     if mode == 2:
-        x = rel + x
+        x += rel
     extend(data, x)
-    data[x] = value
+    data[x] = value  # Position or Relative modes
 
 
 def compute(data, in_value):
@@ -43,31 +40,17 @@ def compute(data, in_value):
                 a, b, c = data[i + 1: i + 4]
                 val_a = get_val(data, a, rel, mode_a)
                 val_b = get_val(data, b, rel, mode_b)
-                if mode_c == 2:
-                    c = rel + c
-                try:
-                    data[c] = val_a + val_b
-                except IndexError:
-                    extend(data, c)
-                    data[c] = val_a + val_b
+                set_val(data, val_a + val_b, c, rel, mode_c)
                 i += 4
             elif op == 2:  # Multiply
                 a, b, c = data[i + 1: i + 4]
                 val_a = get_val(data, a, rel, mode_a)
                 val_b = get_val(data, b, rel, mode_b)
-                if mode_c == 2:
-                    c = rel + c
-                try:
-                    data[c] = val_a * val_b
-                except IndexError:
-                    extend(data, c)
-                    data[c] = val_a * val_b
+                set_val(data, val_a * val_b, c, rel, mode_c)
                 i += 4
             elif op == 3:  # Input
                 a = data[i + 1]
-                if mode_a == 2:
-                    a = rel + a
-                data[a] = in_value
+                set_val(data, in_value, a, rel, mode_a)
                 i += 2
             elif op == 4:  # Output
                 a = data[i + 1]
@@ -88,42 +71,31 @@ def compute(data, in_value):
                 a, b, c = data[i + 1: i + 4]
                 val_a = get_val(data, a, rel, mode_a)
                 val_b = get_val(data, b, rel, mode_b)
-                if mode_c == 2:
-                    c = rel + c
-                try:
-                    data[c] = 1 if val_a < val_b else 0
-                except IndexError:
-                    extend(data, c)
-                    data[c] = 1 if val_a < val_b else 0
+                result = 1 if val_a < val_b else 0
+                set_val(data, result, c, rel, mode_c)
                 i += 4
             elif op == 8:  # Equal to
                 a, b, c = data[i + 1: i + 4]
                 val_a = get_val(data, a, rel, mode_a)
                 val_b = get_val(data, b, rel, mode_b)
-                if mode_c == 2:
-                    c = rel + c
-                try:
-                    data[c] = 1 if val_a == val_b else 0
-                except IndexError:
-                    extend(data, c)
-                    data[c] = 1 if val_a == val_b else 0
+                result = 1 if val_a == val_b else 0
+                set_val(data, result, c, rel, mode_c)
                 i += 4
             elif op == 9:  # Adjust relative base
                 a = data[i + 1]
-                val_a = get_val(data, a, rel, mode_a)
-                rel += val_a
+                rel += get_val(data, a, rel, mode_a)
                 i += 2
             elif op == 99:
                 return output
 
 
 def main():
-    data = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
-    assert compute(data.copy(), 1) == data
-    data = [1102,34915192,34915192,7,4,7,99,0]
-    assert len(str(compute(data.copy(), 1)[0])) == 16
-    data = [104,1125899906842624,99]
-    assert compute(data.copy(), 1) == [1125899906842624]
+    test_a = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+    assert compute(test_a.copy(), 1) == test_a
+    test_b = [1102,34915192,34915192,7,4,7,99,0]
+    assert len(str(compute(test_b, 1)[0])) == 16
+    test_c = [104,1125899906842624,99]
+    assert compute(test_c, 1) == [1125899906842624]
 
     with open('input.txt') as f:
         data = [int(x) for x in next(f).split(',')]
@@ -131,6 +103,7 @@ def main():
     print(f'Part A: {part_a[0]} - BOOST keycode')
     part_b = compute(data.copy(), 2)
     print(f'Part B: {part_b[0]} - Distress signal coordinates')
+
 
 if __name__ == '__main__':
     main()
