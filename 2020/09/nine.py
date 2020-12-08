ef95e1e948c6904f09b20690ddf413db2ad4ba70
Mitch LeBlanc <supermitch@gmail.com>
2020-12-07 21:31:51 -0800

    Refactor 2020 Day 9

--- 2020/09/nine.py
+++ 2020/09/nine.py
@@ -1,20 +1,10 @@
-import sys
-
-
-def main():
-    data = []
-    with open('input.txt') as f:
-        for line in f:
-            parts = line.strip().split()
-            data.append((parts[0], int(parts[1])))
-
+def calc(data):
     i = 0
     acc = 0
     seen = set()
     while True:
         if i in seen:
-            part_a = acc
-            break
+            return 'loop', acc
         else:
             seen.add(i)
         op, val = data[i]
@@ -25,41 +15,35 @@ def main():
             i += 1
         elif op == 'jmp':
             i += val
+        if i == len(data):  # Beyond last op
+            return 'exit', acc
 
-    found = False
-    for idx in range(len(data)):
+
+def main():
+    data = []
+    with open('input.txt') as f:
+        for line in f:
+            parts = line.strip().split()
+            data.append((parts[0], int(parts[1])))
+
+    _, part_a = calc(data)
+
+    for i in range(len(data)):
         copy = data[:]
-        op, val = copy[idx]
+        op, val = copy[i]
         if op == 'nop':
-            copy[idx] = 'jmp', val
+            copy[i] = 'jmp', val
         elif op == 'jmp':
-            copy[idx] = 'nop', val
+            copy[i] = 'nop', val
 
-        i = 0
-        acc = 0
-        seen = set()
-        while True:
-            if i in seen:  # Infinite loop re-starts
-                break
-            else:
-                seen.add(i)
-            op, val = copy[i]
-            if op == 'acc':
-                acc += val
-                i += 1
-            elif op == 'nop':
-                i += 1
-            elif op == 'jmp':
-                i += val
-            if i == len(copy):
-                part_b = acc
-                found = True
-                break
-        if found:
+        code, val = calc(copy)
+        if code == 'exit':
+            part_b = val
             break
 
     print(f'Part A: {part_a} - Acc before infinite loop')
-    print(f'Part B: {part_b} - Acc before routine terminates')
+    print(f'Part B: {part_b} - Acc before routine exits')
+
 
 if __name__ == "__main__":
     main()
