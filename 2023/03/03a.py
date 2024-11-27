11af686de7c22295d0fdf66472518567242675fa
Mitch LeBlanc <supermitch@fastmail.com>
2024-11-27 11:20:53 -0800

    Fix 2023 day 03 bugs

--- 2023/03/03a.py
+++ 2023/03/03a.py
@@ -1,7 +1,7 @@
-with open('03a_test.txt') as f:
+with open('03_input.txt') as f:
     lines = [x.strip() for x in f]
 
-nums = {}
+nums = []
 for y, line in enumerate(lines):
     num = ''
     coords = []
@@ -11,24 +11,29 @@ for y, line in enumerate(lines):
             coords.append((x, y))
         else:
             if num:
-                nums[int(num)] = coords
+                nums.append((int(num), coords))
             num = ''
             coords = []
+    if num:  # On a number when line ends
+        nums.append((int(num), coords))
 
-symbols = ('*', '#', '+', '$')
-valid = set()
-for num, coords in nums.items():
+
+valid = []
+for num, coords in nums:
+    added = False
     for x, y in coords:
         for dx in (-1, 0, 1):
             for dy in (-1, 0, 1):
                 x2, y2 = x + dx, y + dy
                 if (
-                    dx == 0 and dy == 0
+                    (dx == 0 and dy == 0)
                     or x2 < 0 or x2 >= len(lines[0])
                     or y2 < 0 or y2 >= len(lines)
                 ):
                     continue
-                if lines[y2][x2] in symbols:
-                    valid.add(num)
+                if lines[y2][x2] not in '0123456789.':
+                    if not added:
+                        valid.append(num)
+                    added = True
 
 print(f'Part a: {sum(valid)}')
