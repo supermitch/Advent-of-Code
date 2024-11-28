0b42971daa485e3e4457079282d3678117711bdc
Mitch LeBlanc <supermitch@fastmail.com>
2024-11-27 20:19:47 -0700

    Add 2023 day 06b

--- 2023/06/06a.py
+++ 2023/06/06a.py
@@ -6,12 +6,25 @@ with open('06_input.txt') as f:
 
 races = list(zip(times, recs))
 
-ways = []
-for time, record in races:
+
+def calculate_wins(time, record):
     wins = 0
     for charge in range(time):
         if charge * (time - charge) > record:
             wins += 1
+    return wins
+
+
+ways = []
+for time, record in races:
+    wins = calculate_wins(time, record)
     ways.append(wins)
 
 print(f'Part a: {prod(ways)} ways product')
+
+
+time = int(''.join(str(x) for x in times))
+record = int(''.join(str(x) for x in recs))
+wins = calculate_wins(time, record)
+
+print(f'Part b: {wins} ways to win')
